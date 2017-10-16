e26f32289a8795cb65cbbbf6fc7646e87cdabcad
krille <kristofer.westling@gmail.com>
2017-10-16 09:41:13 +0200

    WP Configs

--- README.md
+++ README.md
@@ -17,3 +17,11 @@
 
 ## Development
 `gulp watch` - watches files specified in gulpfile.js for changes and runs the specified tasks on change.
+
+## Wordpress Configs
+Configs are stored in `public/configs` folder.
+1. Manually copy the `wp-config.local.example.php` file to a new file called `wp-config.local.php` and fill-in your own values. This file is for sensitive information and will not be stored in version control.
+2. **Environments:** Set the correct environments you wish to support via the file `wp-config.env.php`. Any `wp-config.{environment}.php` file not used can be removed. Remember to remove the reference in `wp-config.env.php` aswell.
+3. If a setting is the same across all environments (e.g. authentication unique keys and salts), add to `wp-config.default.php`
+
+See https://github.com/studio24/wordpress-multi-env-config for more information.
---
+++ public/configs/wp-config.default.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Default config settings
+ *
+ * Enter any WordPress config settings that are default to all environments
+ * in this file.
+ *
+ * Please note if you add constants in this file (i.e. define statements)
+ * these cannot be overridden in environment config files so make sure these are only set once.
+ *
+ * @package    Studio 24 WordPress Multi-Environment Config
+ * @version    2.0.0
+ * @author     Studio 24 Ltd  <hello@studio24.net>
+ */
+
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'sZc/J4Wk1PB8kuM2hghkcncj8efXs6L1pxUlwk2nkp83r56iV6/WS9byGOXK/PbTBZXm5KrIXxCnaLF3L6SFyQ==');
+define('SECURE_AUTH_KEY',  'MC9vRNgMFtlNkTGsC7Tn/CF6L9aat9hliP4kMTwLWhRktrTjYbunrPDpAdwFMA9fhZ0U4tgwbncITHQwIA6pnQ==');
+define('LOGGED_IN_KEY',    'CXiqppoXDRAZMhhbb7Gqe79Y7ouNyW0vo/qPM2zBm+wGuRm1b8wZEyf71HVJfLWb5OvNEB/i+AA1Jcz/+r1ADg==');
+define('NONCE_KEY',        'b6SSNw1CoKIqXuvhDCxc0WtwCYilhGwwsH3mVczZSrxf8vIraFAM3vXKeaXSfOBEtsNG8nISS6es3f71FOL38Q==');
+define('AUTH_SALT',        'TkF1LBY8DEzOYhEVAOHz4NTMI0/rqegpgvAMDOuqhi90yyMsUARYSLieBLoRUuS4cMBc4hUzjJ8eA1Kr3BmnjQ==');
+define('SECURE_AUTH_SALT', 'QpkzyYt4eVQJ2T/JuUyC6VAJAoireADo4ba9Jd52yvrie7m+Yw20XVts37ilsPTANWobSVeM6WXPL/Xw2iw7vQ==');
+define('LOGGED_IN_SALT',   'LQB2KLxXMoVN+PbYRirriRq+Ze+YnKi6PAYHbYVLzO7zqh3QhgzfBbowXzgvVVCNEmUPlovRsPtG2J5k36AtKA==');
+define('NONCE_SALT',       'nTX9G1PIGv0h4wLy3bh5gMJvNx/d0foa854GFZR1730MPmje7cDg92XhMn4h1TA3BS6jiiicH7ZFmVxaaY9iqw==');
+
+/**#@-*/
+
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', '');
+
+/**
+ * Increase memory limit.
+ */
+define('WP_MEMORY_LIMIT', '64M');
